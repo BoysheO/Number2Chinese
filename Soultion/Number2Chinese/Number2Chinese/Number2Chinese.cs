374f2be0ef1fabc3bad186200cda276e13b1603b
ouboxu <946900978@qq.com>
2023-01-09 11:28:34 +0800

    update

--- Soultion/Number2Chinese/Number2Chinese/Number2Chinese.cs
+++ Soultion/Number2Chinese/Number2Chinese/Number2Chinese.cs
@@ -8,70 +8,129 @@ using BoysheO.Extensions;
 namespace Number2Chinese
 {
     /// <summary>
-    /// 最大单位支持到亿，因为人民生活习惯的计数单位到亿为止,例如会人们通常使用八十万亿而不是八十兆。
+    /// 最大单位支持到极
     /// </summary>
     public static class Number2Chinese
     {
-        public static readonly string LowercaseLetters = "零一二三四五六七八九";
-
-        public static readonly string[] LowercaseUnitLetters =
-        {
-            "十",
-            "百",
-            "千",
-            "万",
-            "十万",
-            "百万",
-            "千万",
-            "亿",
-        };
-
-        // public static ReadOnlySpan<char> GetLowercaseUnit(int unitOrder)
-        // {
-        //     if(unitOrder==0)return Span<char>.Empty;
-        //     if (unitOrder < LowercaseUnitLetters.Length) return LowercaseUnitLetters[unitOrder].AsSpan();
-        //     
-        // }
-
         // public static readonly string UppercaseLetters = "零壹贰叁肆伍陆柒捌玖";
 
-        public static string ToLowercaseString(int number)
-        {
-            bool isLessThan0 = number < 0;
-            if (isLessThan0)
-            {
-                number = -number;
-            }
+        public static readonly string LowercaseLetters = "零一二三四五六七八九";
 
-            Span<char> chars = stackalloc char[10];
-            var charCount = 0;
+        public static readonly char[] LowercaseLittleUnitLetters =
+        {
+            '个',
+            '十',
+            '百',
+            '千',
+        };
+
+
+        public static readonly string[] LowercaseBigUnitLetters =
+        {
+            "个",
+            "万",
+            "亿",
+            "兆",
+            "京",
+            "垓",
+            "秭",
+            "穰",
+            "沟",
+            "涧",
+            "正",
+            "载",
+            "极"
+        };
+
+        /// <summary>
+        /// 按万进中国习惯，按需读零输出读数。
+        /// </summary>
+        public static string ToLowercaseReadingStringInChinese(ulong number)
+        {
+            if (number < 10) return LowercaseLetters[(int)number].ToString();
+
+            #region 拆解数位
+
+            Span<byte> digits = stackalloc byte[20]; //ulong型最大这么多位
+            var digitsCount = 0;
             while (number > 0)
             {
-                var div = Math.DivRem(number, 10, out var mode);
-                chars[charCount] = LowercaseLetters[mode];
-                charCount++;
+                var div = number / 10;
+                var mod = number - div * 10;
+                digits[digitsCount] = unchecked((byte)mod);
+                digitsCount++;
                 number = div;
             }
-            //运行至此，chars里面按逆序放置着数字对应汉字
-            
-            // Span<char> chars2 = stackalloc char[20];
-            // var chars2Count = 0;
-            // for (int i = 0; i < charCount; i++)
-            // {
-            //     var c = chars[i];
-            //     chars2[chars2Count] = c;
-            //     chars2Count++;
-            //     var unit = LowercaseUnitLetters[i].AsSpan();
-            //     for (int j = unit.Length; j >= 0; j++)
-            //     {
-            //         chars2[chars2Count] = unit[j];
-            //         chars2Count++;
-            //     }
-            // }
+
+            digits.Slice(0, digitsCount).Reverse();
+
+            #endregion
+
+            Span<char> chars = stackalloc char[59]; //ulong型最多使用这么多字
+            var charsCount = 0;
+            var p = 0;
+            bool is0LastDigit = false;
+            while (p < digitsCount)
+            {
+                var unitOrder = digitsCount - p - 1;
+                var digit = digits[p];
+
+                //位数上是0时不读
+                if (digit == 0)
+                {
+                    p++;
+                    is0LastDigit = true;
+                    continue;
+                }
+
+                if (is0LastDigit)
+                {
+                    chars[charsCount] = LowercaseLetters[0];
+                    charsCount++;
+                    is0LastDigit = false;
+                }
+
+                var littleUnitOrder = unitOrder % 4;
+                //如果首位是一十则缩略成十
+                if (p == 0 && digit == 1 && littleUnitOrder == 1)
+                {
+                }
+                else
+                {
+                    var digitChar = LowercaseLetters[digit];
+                    chars[charsCount] = digitChar;
+                    charsCount++;
+                }
+
+                if (littleUnitOrder > 0)
+                {
+                    var unit = LowercaseLittleUnitLetters[littleUnitOrder];
+                    chars[charsCount] = unit;
+                    charsCount++;
+                }
+
+                //逢四得到一个大位
+                if (unitOrder % 4 == 0)
+                {
+                    var order = unitOrder / 4;
+                    //忽略个位
+                    if (order > 0)
+                    {
+                        var bigUnit = LowercaseBigUnitLetters[order].AsSpan();
+                        for (int i = 0; i < bigUnit.Length; i++)
+                        {
+                            chars[charsCount] = bigUnit[i];
+                            charsCount++;
+                        }
+                    }
+                }
+
+                p++;
+            }
 
             unsafe
             {
-                fixed (char* c = chars)
+                fixed (char* c = chars.Slice(0,charsCount))
                 {
                     return new string(c);
                 }
--- Soultion/Number2Chinese/Number2ChineseTest/UnitTest1.cs
+++ Soultion/Number2Chinese/Number2ChineseTest/UnitTest1.cs
@@ -2,18 +2,20 @@ namespace Number2ChineseTest;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    [TestCase(1,ExpectedResult = "一")]
-    [TestCase(12,ExpectedResult = "十二")]
-    [TestCase(23,ExpectedResult = "二十三")]
-    [TestCase(123,ExpectedResult = "一百二十三")]
-    public string TestIntToLowercaseString(int number)
+    [TestCase((ulong)0, ExpectedResult = "零")]
+    [TestCase((ulong)1, ExpectedResult = "一")]
+    [TestCase((ulong)10, ExpectedResult = "十")]
+    [TestCase((ulong)12, ExpectedResult = "十二")]
+    [TestCase((ulong)23, ExpectedResult = "二十三")]
+    [TestCase((ulong)1230000, ExpectedResult = "一百二十三万")]
+    [TestCase((ulong)1230001, ExpectedResult = "一百二十三万零一")]
+    [TestCase((ulong)1230012, ExpectedResult = "一百二十三万零一十二")]
+    [TestCase((ulong)1002_3001_0012, ExpectedResult = "一千零二亿三千零一万零一十二")]
+    [TestCase(ulong.MaxValue, ExpectedResult = "一千八百四十四京六千七百四十四兆零七百三十七亿零九百五十五万一千六百一十五")]
+    [TestCase(9999999999999999999, ExpectedResult = "九百九十九京九千九百九十九兆九千九百九十九亿九千九百九十九万九千九百九十九")]
+    public string TestIntToLowercaseString(ulong number)
     {
-        return Number2Chinese.Number2Chinese.ToLowercaseString(number);
+        return Number2Chinese.Number2Chinese.ToLowercaseReadingStringInChinese(number);
     }
 }
